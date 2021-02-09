9e5966eda35e27e3c3a5b33e1dabca9736759d8d
Tomas Sanchez <tomassanchezescobar@gmail.com>
2021-02-09 18:49:25 -0300

    Agregado el carrito y los links

--- project/app.js
+++ project/app.js
@@ -13,7 +13,6 @@ app.get('/', (req,res)=>{
     res.sendFile(path.resolve(__dirname + '/views/index.html'));
 });
 
-/*
 app.get('/login', (req,res)=>{
     res.sendFile(path.resolve(__dirname + '/views/login.html'));
-}); */
+});
