56fede4303eccb5f59434e7e1829de3938b33f58
NFGarilli <garillinicolas@hotmail.com>
2021-02-09 15:53:43 -0300

    arranque el login/register, faltan arreglos de posicion

--- project/app.js
+++ project/app.js
@@ -12,3 +12,8 @@ app.listen(3000, ()=>{
 app.get('/', (req,res)=>{
     res.sendFile(path.resolve(__dirname + '/views/index.html'));
 });
+
+/*
+app.get('/login', (req,res)=>{
+    res.sendFile(path.resolve(__dirname + '/views/login.html'));
+}); */
