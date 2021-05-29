b4035a9e2bb63ffed7261fa0c2dc5ce5b3e81e75
alvarezlucass <alvarezlucass@hotmail.com>
2021-05-29 14:18:23 -0300

    corrigiendo

--- project/src/middlewares/validateLoginMiddleware.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const { body } = require('express-validator');
-
-module.exports = [
-    body('email')
-        .notEmpty().withMessage('Tienes que escribir un email').bail()
-        .isEmail().withMessage('Tienes que escribir un formato de email valido'),
-    body('password').notEmpty().withMessage('Tienes que escribir una contraseña'),
-]
