91f51708dd7042eec75221a55e488d9faa0663ca
alvarezlucass <alvarezlucass@hotmail.com>
2021-06-06 12:49:02 -0300

    detalles

--- api-refactor/src/database/config/config.js
+++ api-refactor/src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": '',
+    "password": "root",
     "database": "g3_refactor",
     "host": "127.0.0.1",
     "dialect": "mysql"
